7ad37c0840de3649c9d6d2e0880121eebfa22736
ViliusZe <viliuszeigis@gmail.com>
2025-02-26 20:06:56 +0200

    primas failas
    struktura sukurta

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iomanip>
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+struct duomenys 
+{
+    string v, p;
+    int paz;
+    int N[5];
+    int e;
+    double r;
+};
+int main()
+{
+    
+    return 0;
+}
